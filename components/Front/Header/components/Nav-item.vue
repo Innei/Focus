834256b71f812347252c6eb557b218904f62ef8e
Innei <tukon479@gmail.com>
2019-12-09 21:17:28 +0800

    navigation v1.

---
+++ components/Front/Header/components/Nav-item.vue
@@ -0,0 +1,55 @@
+<template>
+  <nuxt-link :to="link">
+    <div class="nav-item">
+      <div class="icon" :class="icon">
+        <slot name="svg" />
+      </div>
+
+      <div class="text">
+        {{ text }}
+      </div>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: ['icon', 'text', 'link']
+}
+</script>
+
+<style lang="scss" scoped>
+.nav-item {
+  overflow: hidden;
+  position: relative;
+  .icon {
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+    margin: 12px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 0 0px 1.4px #434d54;
+    float: left;
+  }
+  .text {
+    float: left;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    display: flex;
+    left: 64px;
+    align-items: center;
+    opacity: 0.6;
+    transition: opacity 0.5s;
+    &:hover {
+      opacity: 1;
+    }
+  }
+  &::after {
+    content: '';
+    clear: both;
+  }
+}
+</style>
--- components/Front/Header/index.vue
+++ components/Front/Header/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="header">
-    <Nav />
+    <Nav class="navigation" />
   </div>
 </template>
 
@@ -17,5 +17,25 @@ export default {
   width: (64 * 3) + px;
   top: 0;
   height: 100vh;
+  &::after {
+    content: '';
+    position: absolute;
+    right: 0;
+    height: 100%;
+    top: 0;
+    width: 1px;
+    background-image: linear-gradient(
+      #fff 50%,
+      map-get($map: $colors, $key: 'gray') 150%
+    );
+    opacity: 0.4;
+  }
+}
+.navigation {
+  opacity: 0.6;
+  transition: opacity 0.6s;
+  &:hover {
+    opacity: 1;
+  }
 }
 </style>
