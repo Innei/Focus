cd8c1bbd019ecdf25a428360626f72cb646de6e8
Innei <tukon479@gmail.com>
2019-12-21 21:18:45 +0800

    add

--- components/Front/Note/components/Swiper/index.vue
+++ components/Front/Note/components/Swiper/index.vue
@@ -6,12 +6,12 @@
 import 'swiper/dist/css/swiper.css'
 
 import { swiper } from 'vue-awesome-swiper'
-import cardSlide from '@/components/Home/slide.vue'
+import Slide from './slide'
 
 export default {
   components: {
     swiper,
-    cardSlide
+    Slide
   },
 
   data() {
@@ -31,65 +31,6 @@ export default {
       },
       interval: null
     }
-  },
-  created() {
-    let index = 0
-    if (this.moments.data)
-      this.moments.data.map((item) => {
-        item.index = index++
-      })
-
-    if (this.swiperOption.autoplay) {
-      this.interval = setInterval(() => {
-        this.activeItem++
-        this.$emit('switch', this.activeItem)
-      }, this.swiperOption.delay || 3000)
-    }
-  },
-  mounted() {
-    this.updateSlidesPerview(this.viewport)
-  },
-  watch: {
-    moments(n) {
-      let index = 0
-      n.data.map((item) => {
-        item.index = index++
-      })
-      this.data = n
-      this.$refs.swiper.swiper.slideTo(this.pageOptions.hasPrevPage ? 1 : 0)
-      this.activeItem = 0
-    },
-    viewport(n) {
-      this.updateSlidesPerview(n)
-    }
-  },
-  methods: {
-    updateSlidesPerview(n) {
-      if (n.is568) this.swiperOption.slidesPerView = 2
-      else if (n.is768) this.swiperOption.slidesPerView = 3
-      else if (n.is1024) this.swiperOption.slidesPerView = 4
-      else if (n.is1600) this.swiperOption.slidesPerView = 8
-      else this.swiperOption.slidesPerView = 6
-      this.$refs.swiper.swiper.destroy() //先销毁
-      this.$refs.swiper.mountInstance() //后在加载
-    },
-    parseDate(date) {
-      const d = new Date(date)
-      const dateObj = {
-        date: d.getDate(),
-        my: `${d.getMonth() + 1} / ${d.getFullYear() - 2000}`
-      }
-      return dateObj
-    },
-    handleClick(index) {
-      this.activeItem = index
-      this.$emit('switch', this.activeItem)
-      this.interval = clearInterval(this.interval)
-    }
-  },
-  destroyed() {
-    clearInterval(this.interval)
-    this.interval = null
   }
 }
 </script>
