677d488130b233f8984968669bd9990a51202c9b
Innei <tukon479@gmail.com>
2020-02-04 13:16:36 +0800

    add global element component

--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,9 +1,15 @@
 import 'element-ui/lib/theme-chalk/icon.css'
 import 'element-ui/lib/theme-chalk/message.css'
-import 'element-ui/lib/theme-chalk/backtop.css'
+import 'element-ui/lib/theme-chalk/form.css'
+import 'element-ui/lib/theme-chalk/avatar.css'
+import 'element-ui/lib/theme-chalk/form-item.css'
+import 'element-ui/lib/theme-chalk/input.css'
+import 'element-ui/lib/theme-chalk/button.css'
+
 import Vue from 'vue'
-import { Message } from 'element-ui'
+import { Message, Avatar, Form, FormItem, Input, Button } from 'element-ui'
 Vue.prototype.$message = Message
+;[Avatar, Form, FormItem, Input, Button].map((plugin) => Vue.use(plugin))
 
 // inject to nuxt ctx
 export default ({ app }, inject) => {
